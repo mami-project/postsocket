f84b4555c3a44a3f9dbe89889de8d7302b7549ca
Brian Trammell <brian@trammell.ch>
2018-02-14 09:38:57 +0100

    port to TAPS API (WIP)

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # postsocket
-Go implementation of PostSockets over various transports
+
+This repository contains a sketch of a Go implementation of the Transport
+Services Abstract Interface described in
+[draft-trammell-taps-interface](https://taps-api.github.io/drafts/draft-trammell-taps-interface.html).
+This sketch is currently intended to illustrate issues and design choices made
+during the development of that interface, and as a proof of concept that the
+abstract interface is implementable in Go.
+
+Future revisions of this repository will contain a full implementation of this
+interface backed by TCP, UDP, and a userland QUIC implementation over UDP.
--- api.go
+++ api.go
@@ -7,214 +7,138 @@ import (
 
 ///////////////////////////////////////////////////////////////////////////////
 // Post Sockets API definition
-//
-// This is a work in progress, attempting to track developments in
-// https://datatracker.ietf.org/doc/draft-trammell-taps-post-sockets. See
-// https://github.com/mami-project/postsocket/issues for issues identified
-// with this abstract API that need a solution here and/or in the document.
-///////////////////////////////////////////////////////////////////////////////
+// Transport Services (TAPS) Abstract Interface edition
+/////////////////////////////////////////////////////////////////////////////////
 
-// PostContext encapsulates all the state kept by the Post Sockets API at a
-// single endpoint, and is the "root" of the API. It contains Policies and
-// Configurations governing connection initiation and listening, available
-// Locals, stored Associations and Paths, and currently open Carriers and
-// Transients. An application will generally create a single PostContext
-// instance on startup, optionally Load() state from disk, and can checkpoint
-// in-memory state to disk with Save().
-type PostContext interface {
-	// Initiate a connection from a given Local to a given Remote, with an
-	// optional Configuration to override association- and context-level
-	// Conifigurations, returning a Carrier on which messages can be sent and
-	// received.
-	Initiate(loc Local, rem Remote, cfg Configuration) (Carrier, error)
+// TransportContext encapsulates all the state kept by the API at a single
+// endpoint, and is the "root" of the API. It can be used to create new
+// default transport and security parameters, locals and remotes bound to the
+// available provisioning domains and resolution, as well as new Connections
+// with these properties. It stores path (address pair) and association
+// (endpoint pair) properties and ephemeral as well as durable state. An
+// application will generally create a single TransportContext instance on
+// startup, optionally Load() state from disk, and can checkpoint in-memory
+// state to disk with Save().
+type TransportContext interface {
+	NewTransportParameters() TransportParameters
+	NewSecurityParameters() SecurityParameters
+	NewRemote() Remote
+	NewLocal() Local
 
-	// Listen for connections on a given Local which will pass connection
-	// requests to a given ListenFunc.
-	Listen(loc Local, lfn ListenFunc, cfg Configuration) (Listener, error)
+	DefaultSendParameters() SendParameters
 
-	// Create a new Source associated with a given Local and Remote. Calls
-	// associated with receiving on a Source  will result in a runtime
-	// error.
-	NewSource(loc Local, rem Remote, cfg Configuration) (Carrier, error)
+	// Initiate a connection to a given Remote, with a given Handler for
+	// connection events, with optional local, transport parameters, and
+	// security parameters. Each of the optional arguments may be passed as
+	// nil; if so, the Context defaults are used. Returns a Connection in the
+	// process of being initiated. Once the Connection is initiated, the
+	// ConnectionHandler's Ready callback will be called with this connection
+	// and a nil antecedent.
+	Initiate(ch ConnectionHandler, rem Remote, loc Local, tp TransportParameters, sp SecurityParameters) (Connection, error)
 
-	// Create a new Sink associated with a given Local. Calls
-	// associated with sending on a Sink will result in a runtime
-	// error.
-	NewSink(loc Local, cfg Configuration) (Carrier, error)
+	// Rendezvous with a given Remote using an appropriate peer to peer
+	// rendezvous method, with a given Handler for connection events, with
+	// optional local, transport parameters, and security parameters. Each of
+	// the optional arguments may be passed as nil; if so, the Context
+	// defaults are used. Returns a Connection in the process of being
+	// rendezvoused. The ConnectionHandler's Ready callback will be called
+	// with any established Connection(s), with this Connection as antecedent.
+	Rendezvous(ch ConnectionHandler, rem Remote, loc Local, tp TransportParameters, sp SecurityParameters) (Connection, error)
 
-	// Save this context's state to a file on disk.
+	// Listen on a given Local with a given Handler for connection events,
+	// with optional transport and security parameters. Each of the optional
+	// arguments may be passed as nil; if so, the Context defaults are used.
+	// Returns a Listener in the process of being started. The
+	// ConnectionHandler's Ready callback will be called with any accepted
+	// Connection(s), with this Connection as antecedent.
+	Listen(ch ConnectionHandler, loc Local, tp TransportParameters, sp SecurityParameters) (Connection, error)
+
+	// Save this context's state to a file on disk. The format of this state
+	// file is not specified and not necessarily portable across
+	// implementations of the API.
 	Save(filename string) error
 
-	// Replace this context's state with state loaded from a file on disk.
-	Load(filename string) error
-
-	// Bind a configuration to this context
-	Configure(cfg Configuration) error
-
-	// Retrieve the configuration in effect for this context.
-	Configuration() Configuration
-
-	// Iterate over currently active provisioning domains.
-	ProvisioningDomains() []ProvisioningDomain
-
-	// Create a new Local from a string specification, optionally bound to a
-	// provisioning domain, optionally identified by zero or more TLS
-	// certificates.
-	NewLocal(spec string, pvd ProvisioningDomain, identities []tls.Certificate) (Local, error)
-
-	// Create a new Remote from a string specification. The Remote will be
-	// optionally scoped for use with a given Local during resolution, and
-	// optionally subject to a given identity check on any identity presented.
-	NewRemote(spec string, loc Local, identityCheck IdentityCheckFn)
+	// Replace this context's state with state loaded from a file on disk. The
+	// format of this state file is not specified and not necessarily portable
+	// across implementations of the API.
+	Restore(filename string) error
 }
 
-// Association encapsulates state about communications between a local
-// endpoint and a remote endpoint over a set of paths. It includes local and
-// remote identity information, cached cryptographic resumption parameters and
-// cached path properties.
-type Association interface {
+type TransportParameter int
 
-	// Iterate over the Carriers currently bound to this Association
-	Carriers() []Carrier
+const (
+	TransportFullyReliable = iota
+	// ... and so on
+)
 
-	// Iterate over the Paths this Association has properties for. Not all
-	// paths are presently in use.
-	Paths() []Path
+type SecurityParameter int
 
-	// Initiate a new Carrier on this Association
-	Initiate() (Carrier, error)
+const (
+	SecuritySupportedGroup = iota
+	SecurityCiphersuite
+	SecuritySignatureAlgorithm
+	// ... and so on
+)
 
-	// Bind a configuration to this Association. Association-level
-	// configurations override context-level configurations where permitted by
-	// policy.
-	Configure(cfg Configuration) error
-
-	// Return the Configuration in effect for this Association. This
-	// configuration will include configuration directives inherited from the
-	// PostContext in which the association was created.
-	Configuration() Configuration
+type TransportParameters interface {
+	Require(p TransportParameter, v int) TransportParameters
+	Prefer(p TransportParameter, v int) TransportParameters
+	Avoid(p TransportParameter, v int) TransportParameters
+	Prohibit(p TransportParameter, v int) TransportParameters
 }
 
-// Carrier provides an interface over which messages can be sent to and
-// received from a remote endpoint.
-type Carrier interface {
-	// Access the association backing this Carrier
-	Association() Association
-
-	// Iterate over the Paths this Carrier is currently using.
-	Paths() []Path
-
-	// Close this Carrier
-	Close() error
-
-	// Register an event handler to be notified when this carrier closes
-	OnClosed(fn CloseEventFunc)
-
-	// Send a Message on this carrier with given lifetime, niceness,
-	// idempodence, and immediacy
-	Send(msg []byte, lifetime int, nice int, idem bool, immed bool) error
-
-	// Register an event handler to be notified when a message expires before
-	// being sent
-	OnExpired(fn SendEventFunc) error
-
-	// Register an event handler to be notified when a message is sent
-	OnSent(fn SendEventFunc) error
-
-	// Register an event handler to be notified when a message is acknowledged
-	OnAcked(fn SendEventFunc) error
-
-	// Register a receiver that will be called when a message is received
-	Ready(fn ReceiveFunc) error
-
-	// Register an event handler that will be called when a receive error occurs
-	OnReceiveError(fn ReceiveErrorFunc)
-
-	// Register a deframer
-	DeframeWith(fn DeframeFunc)
+type SecurityParameters interface {
+	AddIdentity(c tls.Certificate) SecurityParameters
+	AddPSK(c tls.Certificate, k []byte) SecurityParameters
+	VerifyTrustWith(func() (bool, error)) SecurityParameters     // FIXME needs useful args
+	HandleChallengeWith(func() (bool, error)) SecurityParameters // FIXME needs useful args
+	Require(p SecurityParameter, v int) SecurityParameters
+	Prefer(p SecurityParameter, v int) SecurityParameters
+	Avoid(p SecurityParameter, v int) SecurityParameters
+	Prohibit(p SecurityParameter, v int) SecurityParameters
 }
 
-// ProvisioningDomain contains information about a single connection this
-// endpoint has to remote networks and/or the Internet.
-//
-// FIXME the methods defined here may actually be implementation-specific;
-// in this case, the PvD in the abstract API might be an interface{}.
-type ProvisioningDomain interface {
-	// Create a Local bound to this Provisioning Domain; i.e., that will only
-	// use the interface it encapsulates.
-	LocalFor(spec string, identities []tls.Certificate)
-
-	// Determine whether a given resolved Remote is reachable using this PvD
-	CanReach(rem Remote) bool
+type SendParameters struct {
+	ContentRef         interface{}
+	Lifetime           uint
+	Niceness           uint
+	Ordered            bool
+	Immediate          bool
+	Idempotent         bool
+	CorruptionTolerant bool
 }
 
-// Local represents information about a single local endpoint, i.e. to which a
-// Listener can be bound, and which may have a coherent security identity.
-type Local interface {
-	// Return a canonicalized string representing this Local's specification.
-	String() string
-	// Return the certificates by which this Local identifies itself
-	Identities() []tls.Certificate
+type Content interface {
+	Bytes() []byte
 }
 
 type Remote interface {
-	// Return a canonicalized string representing this Remote's specification.
-	String() string
-
-	// Return the certificates by which this Remote identifies itself
-	Identities() []tls.Certificate
-
-	// Resolve this Remote to a further stage of resolution, optionally scoping that resolution
-	Resolve(loc Local) ([]Remote, error)
-
-	// Return the Remote from which this one was resolved, or nil if the Remote is not the result of resolution.
-	Antecedent() Remote
-
-	// Determine whether this Remove can be used for establishing a connection, or requires further resolution to do so
-	Complete() bool
 }
 
-type Path interface {
-	// Return the Local associated with this Path
-	Local() Local
-
-	// Return the Remote associated with this Path
-	Remote() Remote
-
-	// Return the current value of a named path property FIXME replace with core path properties
-	Get(property string) interface{}
+type Local interface {
 }
 
-type Configuration interface {
-	// Get the value of a named configuration property
-	Get(key string) interface{}
-
-	// Set the value of a named configuration property to a given value
-	Set(key string, value interface{})
+type Connection interface {
+	Send(c interface{}, sp SendParameters) error
+	Clone() (Connection, error)
 }
 
 type Listener interface {
-	// Close this listener; i.e., stop listening
-	Close() error
 }
 
-type ListenFunc func()
+type ContentRef interface {
+}
 
-// DeframeFunc is used to deframe a byte stream into discrete messages. When
-// Post Sockets is used with a transport protocol which does not support
-// message boundary preservation, this allows an application to push message
-// deframing down into the API implementation. DeframeFunc, when called,
-// should read a single application-layer message into a byte slice, and leave
-// the reader at the stream position of the start of the next message, if any.
-type DeframeFunc func(in io.Reader) ([]byte, error)
-
-type ReceiveFunc func(msg []byte, carrier Carrier)
-
-type ReceiveErrorFunc func(err error)
-
-// SendEventFunc is a callback type for events on a sent message.
-type SendEventFunc func(msg []byte, carrier Carrier)
-
-type CloseEventFunc func(carrier Carrier)
-
-type IdentityCheckFn func(certs []tls.Certificate) error
+type ConnectionHandler struct {
+	Ready           func(conn Connection, ante Connection)
+	Received        func(content Content, conn Connection)
+	Sent            func(conn Connection, cr interface{})
+	Expired         func(conn Connection, cr interface{})
+	SendError       func(conn Connection, err error)
+	ReceiveError    func(conn Connection, err error)
+	InitiateError   func(conn Connection, err error)
+	RendezvousError func(conn Connection, err error)
+	ListenError     func(conn Connection, err error)
+	Frame           func(content interface{}) ([]byte, error)
+	Deframe         func(in io.Reader) (Content, error)
+}
