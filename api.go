ba964588fd4c7c6888b241ec75a2c355bb2d57cd
Brian Trammell <brian@trammell.ch>
2018-02-15 16:51:08 +0100

    API mostly complete, godoc wip

--- api.go
+++ api.go
@@ -4,6 +4,7 @@ import (
 	"crypto/tls"
 	"io"
 	"net"
+	"time"
 )
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -21,6 +22,7 @@ import (
 // startup, optionally Load() state from disk, and can checkpoint in-memory
 // state to disk with Save().
 type TransportContext interface {
+
 	// NewTransportParameters creates a new TransportParameters object with
 	// system and user defaults for this TransportContext. The specification
 	// of system and user defaults is implementation-specific.
@@ -38,22 +40,25 @@ type TransportContext interface {
 	// this TransportContext.
 	NewLocal() Local
 
-	// DefaultSendParameters returns a SendParameters object with
+	// DefaultSendParameters returns a SendParameters object with default values.
 	DefaultSendParameters() SendParameters
 
-	// Initiate a connection to a given Remote, with a given Handler for
-	// connection events, with optional local specifier, transport parameters,
-	// and security parameters. Each of the optional arguments may be passed
-	// as nil; if so, the Context defaults are used. Returns a Connection in
-	// the process of being initiated. Once the Connection is initiated, the
+	// Preconnect creates a Preconnection, a container for sets of related
+	// remote, local, transport and security parameters (a Connection
+	// specifier), which can be instantiated into a Connection. Preconnection
+	// allows the specification of multiple, disjoint sets of related
+	// parameters for candidate transport protocol selection, as well as the
+	// ability to initiate and send atomically for 0-RTT connection. The
+	// Preconnection is initialized with one set of parameters; use Preconnection.
+	Preconnect(ch ConnectionHandler, rem Remote, loc Local, tp TransportParameters, sp SecurityParameters) (Preconnection, error)
+
+	// Initiate a connection with a given handler, remote, local, and
+	// parameters. Any of these except Remote may be nil, in which case
+	// context defaults will be used. Once the Connection is initiated, the
 	// ConnectionHandler's Ready callback will be called with this connection
-	// and a nil antecedent.
-	Initiate(
-		ch ConnectionHandler,
-		rem Remote,
-		loc Local,
-		tp TransportParameters,
-		sp SecurityParameters) (Connection, error)
+	// and a nil antecedent. This is a shortcut for creating a Preconnection
+	// with a single connection specifier and initiating it.
+	Initiate(ch ConnectionHandler, rem Remote, loc Local, tp TransportParameters, sp SecurityParameters) (Connection, error)
 
 	// Rendezvous with a given Remote using an appropriate peer to peer
 	// rendezvous method, with a given Handler for connection events, with
@@ -61,13 +66,10 @@ type TransportContext interface {
 	// the optional arguments may be passed as nil; if so, the Context
 	// defaults are used. Returns a Connection in the process of being
 	// rendezvoused. The ConnectionHandler's Ready callback will be called
-	// with any established Connection(s), with this Connection as antecedent.
-	Rendezvous(
-		ch ConnectionHandler,
-		rem Remote,
-		loc Local,
-		tp TransportParameters,
-		sp SecurityParameters) (Connection, error)
+	// with any established Connection(s), with a nil antecedent.
+	// This is a shortcut for creating a Preconnection with a single
+	// connection specifier and rendezvousing with it.
+	Rendezvous(ch ConnectionHandler, rem Remote, loc Local, tp TransportParameters, sp SecurityParameters) (Connection, error)
 
 	// Listen on a given Local with a given Handler for connection events,
 	// with optional transport and security parameters. Each of the optional
@@ -75,11 +77,7 @@ type TransportContext interface {
 	// Returns a Listener in the process of being started. The
 	// ConnectionHandler's Ready callback will be called with any accepted
 	// Connection(s), with this Connection as antecedent.
-	Listen(
-		ch ConnectionHandler,
-		loc Local,
-		tp TransportParameters,
-		sp SecurityParameters) (Connection, error)
+	Listen(ch ConnectionHandler, loc Local, tp TransportParameters, sp SecurityParameters) (Connection, error)
 
 	// Save this context's state to a file on disk. The format of this state
 	// file is not specified and not necessarily portable across
@@ -90,48 +88,6 @@ type TransportContext interface {
 	// format of this state file is not specified and not necessarily portable
 	// across implementations of the API.
 	Restore(filename string) error
-}
-
-type ParameterIdentifier int
-
-const (
-	TransportFullyReliable = iota
-	SecuritySupportedGroup
-	SecurityCiphersuite
-	SecuritySignatureAlgorithm
-	// ... and so on
-)
-
-type TransportParameters interface {
-	Require(p ParameterIdentifier, v int) TransportParameters
-	Prefer(p ParameterIdentifier, v int) TransportParameters
-	Avoid(p ParameterIdentifier, v int) TransportParameters
-	Prohibit(p ParameterIdentifier, v int) TransportParameters
-}
-
-type SecurityParameters interface {
-	AddIdentity(c tls.Certificate) SecurityParameters
-	AddPSK(c tls.Certificate, k []byte) SecurityParameters
-	VerifyTrustWith(func() (bool, error)) SecurityParameters     // FIXME needs useful args
-	HandleChallengeWith(func() (bool, error)) SecurityParameters // FIXME needs useful args
-	Require(p ParameterIdentifier, v int) SecurityParameters
-	Prefer(p ParameterIdentifier, v int) SecurityParameters
-	Avoid(p ParameterIdentifier, v int) SecurityParameters
-	Prohibit(p ParameterIdentifier, v int) SecurityParameters
-}
-
-type SendParameters struct {
-	ContentRef         interface{}
-	Lifetime           uint
-	Niceness           uint
-	Ordered            bool
-	Immediate          bool
-	Idempotent         bool
-	CorruptionTolerant bool
-}
-
-type Content interface {
-	Bytes() []byte
 }
 
 // Remote specifies a remote endpoint by hostname, address, port, and/or
@@ -178,15 +134,170 @@ type Local interface {
 	WithServiceName(svc string) Local
 }
 
+// ParameterIdentifier identifies a Transport or Security Parameter
+type ParameterIdentifier int
+
+// List of transport and security parameter names. FIXME reference to documentation
+const (
+	TransportFullyReliable = iota
+	SecuritySupportedGroup
+	SecurityCiphersuite
+	SecuritySignatureAlgorithm
+	// ... and so on
+)
+
+// TransportParameters contains a set of parameters used in the selection of
+// transport protocol stacks and paths during connection pre-establishment.
+// Get a new TransportParameters bound to a context with
+// NewTransportParameters(), then set preferences through the Require(),
+// Prefer(), Avoid(), and Prohibit() methods.
+type TransportParameters interface {
+
+	// Require protocols and paths selected to fulfill this parameter. If
+	// no protocols and paths available fulfill this parameter, then no
+	// connection is possible. v is an optional value whose meaning is
+	// parameter-specific.
+	Require(p ParameterIdentifier, v int) TransportParameters
+
+	// Prefer protocols and paths selected to fulfill this parameter.
+	// Preferences are considered after requirements and prohibitions. v is an
+	// optional value whose meaning is parameter-specific.
+	Prefer(p ParameterIdentifier, v int) TransportParameters
+
+	// Avoid protocols and paths selected to fulfill this parameter.
+	// Avoidences are considered after requirements and prohibitions. v is an
+	// optional value whose meaning is parameter-specific.
+	Avoid(p ParameterIdentifier, v int) TransportParameters
+
+	// Prohibit the selection of protocols and paths that fulfill this
+	// parameter. If the protocols and paths available all fulfill this
+	// parameter, then no connection is possible. v is an optional value
+	// whose meaning is parameter-specific.
+	Prohibit(p ParameterIdentifier, v int) TransportParameters
+}
+
+// SecurityParameters contains a set of parameters used in the establishment
+// of security associations. Get a new SecurityParameters bound to a context
+// with NewSecurityParameters(), then set preferences and associate identity
+// and callbacks through the methods on the object.
+type SecurityParameters interface {
+
+	// AddIdentity adds an local identity (as an X.509 certificate with
+	// private key) to this parameter set.
+	AddIdentity(c tls.Certificate) SecurityParameters
+
+	// AddPSK adds an preshared key associated with the given certificate to
+	// the parameter set.
+	AddPSK(c tls.Certificate, k []byte) SecurityParameters
+
+	// VerifyTrustWith registers a callback to verify trust. This callback
+	// takes a certificate and returns true if the certificate is trusted.
+	VerifyTrustWith(func(c tls.Certificate) (bool, error)) SecurityParameters
+
+	// HandleChallengeWith registers a callback to handle identity challenges.
+	// FIXME needs useful args
+	HandleChallengeWith(func() (bool, error)) SecurityParameters
+
+	// Require any established security association to fulfill this parameter.
+	// If no available security associations fulfill this parameter, then no
+	// connection is possible. v is an optional value whose meaning is
+	// parameter-specific.
+	Require(p ParameterIdentifier, v int) SecurityParameters
+
+	// Prefer to establish security associations that fulfill this parameter.
+	// Preferences are considered after requirements and prohibitions. v is an
+	// optional value whose meaning is parameter-specific.
+	Prefer(p ParameterIdentifier, v int) SecurityParameters
+
+	// Avoid the establishment of security associations that fulfill this parameter.
+	// Avoidences are considered after requirements and prohibitions. v is an
+	// optional value whose meaning is parameter-specific.
+	Avoid(p ParameterIdentifier, v int) SecurityParameters
+
+	// Prohibit the establishment of security associations that fulfill this
+	// parameter. If the security associations available all fulfill this
+	// parameter, then no connection is possible. v is an optional value
+	// whose meaning is parameter-specific.
+	Prohibit(p ParameterIdentifier, v int) SecurityParameters
+}
+
+// SendParameters contains a set of parameters used for sending content.
+// DefaultSendParameters() returns the defaults for this context.
+type SendParameters struct {
+	// ContentRef is an opaque object referencing a Content that will be
+	// passed on events pertaining to the Content.
+	ContentRef interface{}
+	// Lifetime after which the object is no longer relevant. Used for
+	// unreliable and partially reliable transports; set to zero or less to
+	// specify fully reliable transport, if available.
+	Lifetime time.Duration
+	// Niceness is the inverse priority of this Content relative to others on
+	// this Connection or within this ConnectionGroup. Niceness 0 messages are
+	// the highest priority.
+	Niceness uint
+	// Ordered is true if the Content must be sent before the next Content
+	// sent on this Connection
+	Ordered bool
+	// Immediate is true if this Content should not be held for coalescing
+	// with other Content in a transport-layer datagram.
+	Immediate bool
+	// Idempotent is true if this Content may be sent to the application more
+	// than once without ill effects. Use this together with SendInitial() for
+	// 0RTT session resumption.
+	Idempotent bool
+	// CorruptionTolerant is true if this Content may be sent to the
+	// application even if checksums fail; it is used to explicitly disable
+	// checksums on sent content.
+	CorruptionTolerant bool
+}
+
+// Preconnection is a container for sets of related remote, local, transport
+// and security parameters (a Connection specifier), which can be instantiated
+// into a Connection. Use Preconnect() to create one with an initial
+type Preconnection interface {
+
+	// AddSpecifier adds a related set of remote, local, transport and
+	// security parameters to this Preconnection.
+	AddSpecifier(rem Remote, loc Local, tp TransportParameters, sp SecurityParameters)
+
+	// Initiate a Connection with a Remote specified by this Preconnection,
+	// using the Local and parameters supplied. Returns a connection in the
+	// initiation process. Once the Connection is initiated, the
+	// ConnectionHandler's Ready callback will be called with this connection
+	// and a nil antecedent.
+	Initiate() (Connection, error)
+
+	// Initiate a Connection with a Remote specified by this Preconnection,
+	// using the Local and parameters supplied, while simultaneously sending
+	// Content with the given SendParameters. Returns a connection in the
+	// initiation process. Once the Connection is initiated, the
+	// ConnectionHandler's Ready callback will be called with this connection
+	// and a nil antecedent.
+	InitialSend(content interface{}, sp SendParameters) (Connection, error)
+
+	// Rendezvous  using an appropriate peer to peer rendezvous method with a
+	// Remote specified by this Preconnection, using the Local and parameters
+	// supplied. Returns a connection in the rendezvous process. The
+	// ConnectionHandler's Ready callback will be called with any established
+	// Connection(s), with a nil antecedent.
+	Rendezvous() (Connection, error)
+
+	// Listen for connections on the Local specified by this Preconnection
+	// using the Local and parameters supplied. Returns a Listener in the
+	// process of being started. The ConnectionHandler's Ready callback will
+	// be called with any accepted Connection(s), with this Connection as
+	// antecedent.
+	Listen() (Connection, error)
+}
+
 type Connection interface {
 	Send(c interface{}, sp SendParameters) error
 	Clone() (Connection, error)
+	Close() error
 }
 
-type Listener interface {
-}
-
-type ContentRef interface {
+type Content interface {
+	Bytes() []byte
 }
 
 type ConnectionHandler struct {
@@ -195,6 +306,7 @@ type ConnectionHandler struct {
 	Sent     func(conn Connection, contentref interface{})
 	Expired  func(conn Connection, contentref interface{})
 	Error    func(conn Connection, contentref interface{}, err error)
+	Closed   func(conn Connection)
 	Frame    func(content interface{}) ([]byte, error)
 	Deframe  func(in io.Reader) (Content, error)
 }
