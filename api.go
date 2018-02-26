a35ed649e8432a9315070800e007727f432486dc
Brian Trammell <brian@trammell.ch>
2018-02-26 22:07:05 +0100

    update to pre-00

--- api.go
+++ api.go
@@ -17,6 +17,7 @@
 package postsocket
 
 import (
+	"crypto"
 	"crypto/tls"
 	"io"
 	"net"
@@ -164,7 +165,41 @@ type ParameterIdentifier int
 // List of transport and security parameter names.
 const (
 	TransportFullyReliable = iota
-	// ... and so on, FIXME fill this in. until then, see document for details
+	TransportOrderPreserved
+	TransportPerMessageReliable
+	TransportIdempotent0RTT
+	TransportMultistreaming
+	TransportTimeoutNegotiationSupport
+	TransportExtendedErrorSupport
+	TransportChecksumControl
+	TransportInterfaceType
+	TransportCapacityProfile
+	TransportTimeout
+	TransportSuggestTimeout
+	TransportRetransmissionThreshold
+	TransportMinimumReceiveChecksumCoverage
+	TransportGroupTransmissionScheduler
+	TransportMaxIdempotent0RTT
+	TransportMaxNoFragment
+	TransportMaxNonpartialSend
+	TransportMaxNonpartialReceive
+	SecuritySupportedGroup
+	SecurityCiphersuite
+	SecuritySignatureAlgorithm
+	SecuritySessionCacheCapacity
+	SecuritySessionCacheLifetime
+	SecuritySessionCacheReuse
+)
+
+// CapacityProfile identifies a capacity profile value
+type CapacityProfile int
+
+// List of supported CapacityProfile values
+const (
+	CapProfDefault = iota
+	CapProfInteractive
+	CapProfConstantRate
+	CapProfBulk
 )
 
 // TransportParameters contains a set of parameters used in the selection of
@@ -174,27 +209,56 @@ const (
 // Prefer(), Avoid(), and Prohibit() methods.
 type TransportParameters interface {
 
-	// Require protocols and paths selected to fulfill this parameter. If
-	// no protocols and paths available fulfill this parameter, then no
-	// connection is possible. v is an optional value whose meaning is
-	// parameter-specific.
+	// Require protocols and paths selected to fulfill this parameter. Returns
+	// a new TransportParameters with this requirement added. If no protocols
+	// and paths available fulfill this parameter, then no connection is
+	// possible. v is an optional value whose meaning is parameter-specific.
 	Require(p ParameterIdentifier, v interface{}) TransportParameters
 
-	// Prefer protocols and paths selected to fulfill this parameter.
-	// Preferences are considered after requirements and prohibitions. v is an
-	// optional value whose meaning is parameter-specific.
+	// Prefer protocols and paths selected to fulfill this parameter. Returns
+	// a new TransportParameters with this preference added. Preferences are
+	// considered after requirements and prohibitions. v is an optional value
+	// whose meaning is parameter-specific.
 	Prefer(p ParameterIdentifier, v interface{}) TransportParameters
 
-	// Avoid protocols and paths selected to fulfill this parameter.
-	// Avoidences are considered after requirements and prohibitions. v is an
-	// optional value whose meaning is parameter-specific.
+	// Ignore this parameter when selecting protocols and paths. Returns a new
+	// TransportParameters with this ignorance added. This is used to cancel
+	// system defaults that may consider this parameter.
+	Ignore(p ParameterIdentifier) TransportParameters
+
+	// Avoid protocols and paths selected to fulfill this parameter. Returns a
+	// new TransportParameters with this avoidance added. Avoidences are
+	// considered after requirements and prohibitions. v is an optional value
+	// whose meaning is parameter-specific.
 	Avoid(p ParameterIdentifier, v interface{}) TransportParameters
 
 	// Prohibit the selection of protocols and paths that fulfill this
-	// parameter. If the protocols and paths available all fulfill this
-	// parameter, then no connection is possible. v is an optional value
-	// whose meaning is parameter-specific.
+	// parameter. Returns a new TransportParameters with this prohibition
+	// added.  If the protocols and paths available all fulfill this
+	// parameter, then no connection is possible. v is an optional value whose
+	// meaning is parameter-specific.
 	Prohibit(p ParameterIdentifier, v interface{}) TransportParameters
+
+	// Get retrieves the current value of a given transport parameter by
+	// parameter identifier. Returns an error if the parameter identifier is
+	// not gettable for this set of transport parameters. This is used to
+	// query read-only and read-write properties on established Connections,
+	// as well as to determine which Parameters were selected during
+	// connection establishment.
+	Get(p ParameterIdentifier) (interface{}, error)
+
+	// Set sets the value of a given settable transport parameter by parameter
+	// identifier. Returns an error if this parameter identifier is not
+	// settable for this set of transport parameters, or if the type of the
+	// given value is not appropriate for the transport parameter.
+	Set(p ParameterIdentifier, v interface{}) error
+}
+
+// SecurityMetadata specifies information about a security association for
+// trust verification and identity challenge callbacks.
+type SecurityMetadata struct {
+	Certificate tls.Certificate
+	// and so on...
 }
 
 // SecurityParameters contains a set of parameters used in the establishment
@@ -203,70 +267,68 @@ type TransportParameters interface {
 // and callbacks through the methods on the object.
 type SecurityParameters interface {
 
-	// AddIdentity adds an local identity (as an X.509 certificate with
-	// private key) to this parameter set.
+	// AddIdentity adds an local identity (as a TLS certificate) to this parameter set.
 	AddIdentity(c tls.Certificate) SecurityParameters
 
-	// AddPSK adds an preshared key associated with the given certificate to
+	// AddPrivateKey adds a public/private key pair to this parameter set.
+	AddPrivateKey(sk crypto.PrivateKey, pk crypto.PublicKey) SecurityParameters
+
+	// AddPSK adds an preshared key associated with a given identity (as a string) to
 	// the parameter set.
-	AddPSK(c tls.Certificate, k []byte) SecurityParameters
+	AddPreSharedKey(key []byte, identity string) SecurityParameters
 
 	// VerifyTrustWith registers a callback to verify trust. This callback
-	// takes a certificate and returns true if the certificate is trusted.
-	VerifyTrustWith(func(c tls.Certificate) (bool, error)) SecurityParameters
+	// takes metadata about a security association and returns true if the
+	// association is trusted.
+	VerifyTrustWith(func(m SecurityMetadata) (bool, error)) SecurityParameters
 
 	// HandleChallengeWith registers a callback to handle identity challenges.
-	// FIXME needs useful args
-	HandleChallengeWith(func() (bool, error)) SecurityParameters
+	// This callback takes metadata about a security association and returns
+	// true if the association is trusted.
+	HandleChallengeWith(func(m SecurityMetadata) (bool, error)) SecurityParameters
 
-	// Require any established security association to fulfill this parameter.
-	// If no available security associations fulfill this parameter, then no
-	// connection is possible. v is an optional value whose meaning is
-	// parameter-specific.
-	Require(p ParameterIdentifier, v interface{}) SecurityParameters
+	// Get retrieves the current value of a given security parameter by
+	// parameter identifier. Returns an error if the parameter identifier is
+	// not gettable for this set of security parameters. This is used to
+	// query read-only and read-write properties on established Connections,
+	// as well as to determine which Parameters were selected during
+	// session establishment.
+	Get(p ParameterIdentifier) (interface{}, error)
 
-	// Prefer to establish security associations that fulfill this parameter.
-	// Preferences are considered after requirements and prohibitions. v is an
-	// optional value whose meaning is parameter-specific.
-	Prefer(p ParameterIdentifier, v interface{}) SecurityParameters
-
-	// Avoid the establishment of security associations that fulfill this parameter.
-	// Avoidences are considered after requirements and prohibitions. v is an
-	// optional value whose meaning is parameter-specific.
-	Avoid(p ParameterIdentifier, v interface{}) SecurityParameters
-
-	// Prohibit the establishment of security associations that fulfill this
-	// parameter. If the security associations available all fulfill this
-	// parameter, then no connection is possible. v is an optional value
-	// whose meaning is parameter-specific.
-	Prohibit(p ParameterIdentifier, v interface{}) SecurityParameters
+	// Set sets the value of a given settable security parameter by parameter
+	// identifier. Returns an error if this parameter identifier is not
+	// settable for this set of security parameters, or if the type of the
+	// given value is not appropriate for the transport parameter.
+	Set(p ParameterIdentifier, v interface{}) error
 }
 
-// SendParameters contains a set of parameters used for sending content.
+// SendParameters contains a set of parameters used for sending Messages.
 // DefaultSendParameters() returns the defaults for this context.
 type SendParameters struct {
 	// Lifetime after which the object is no longer relevant. Used for
 	// unreliable and partially reliable transports; set to zero or less to
 	// specify fully reliable transport, if available.
 	Lifetime time.Duration
-	// Niceness is the inverse priority of this Content relative to others on
+	// Niceness is the inverse priority of this Message relative to others on
 	// this Connection or within this ConnectionGroup. Niceness 0 messages are
 	// the highest priority.
 	Niceness uint
-	// Ordered is true if the Content must be sent before the next Content
+	// Ordered is true if the Message must be sent before the next Message
 	// sent on this Connection
 	Ordered bool
-	// Immediate is true if this Content should not be held for coalescing
-	// with other Content in a transport-layer datagram.
+	// Immediate is true if this Message should not be held for coalescing
+	// with other Messages in a transport-layer datagram.
 	Immediate bool
-	// Idempotent is true if this Content may be sent to the application more
+	// Idempotent is true if this Message may be sent to the application more
 	// than once without ill effects. Use this together with SendInitial() for
 	// 0RTT session resumption.
 	Idempotent bool
-	// CorruptionTolerant is true if this Content may be sent to the
+	// CorruptionTolerant is true if this Message may be sent to the
 	// application even if checksums fail; it is used to explicitly disable
-	// checksums on sent content.
+	// checksums on sent Message.
 	CorruptionTolerant bool
+	// CapacityProfile is the capacity profile to use for this message only
+	CapacityProfile CapacityProfile
 }
 
 // Preconnection is a container for sets of related remote, local, transport
@@ -286,12 +348,13 @@ type Preconnection interface {
 	Initiate() (Connection, error)
 
 	// Initiate a Connection with a Remote specified by this Preconnection,
-	// using the Local and parameters supplied, while simultaneously sending
-	// Content with the given SendParameters. Returns a connection in the
-	// initiation process. Once the Connection is initiated, the
-	// EventHandler's Ready callback will be called with this connection
-	// and a nil antecedent.
-	InitialSend(content interface{}, sp SendParameters) (Connection, error)
+	// using the Local and parameters supplied, while simultaneously sending a
+	// Message with the given SendParameters. Returns a connection in the
+	// initiation process. IntialSend may be called more than once on a given
+	// connection to send multiple Messages during initiation. Once the
+	// Connection is initiated, the EventHandler's Ready callback will be
+	// called with this connection and a nil antecedent.
+	InitialSend(message interface{}, sp SendParameters) (Connection, error)
 
 	// Rendezvous  using an appropriate peer to peer rendezvous method with a
 	// Remote specified by this Preconnection, using the Local and parameters
@@ -312,16 +375,24 @@ type Preconnection interface {
 // connection will be passed to its associated EventHandler.
 type Connection interface {
 
-	// Send sends some content on this connection, with an optional content
-	// reference, an object that will be used to refer to the content on any
+	// Send sends a Message on this connection, with an optional message
+	// reference, an object that will be used to refer to the Message on any
 	// event related to it, and a set of send parameters to govern how it will
-	// be sent. If content is a []byte, the bytes it contains will be sent to
-	// over the connection as a single Content. If content implements the
-	// Content interface, the Bytes() method will be invoked and the resulting
-	// []byte will be transmitted. Otherwise, content will be passed to the
-	// framing handler's Frame() method to convert it to a []byte for
-	// transmission.
-	Send(content interface{}, contentref interface{}, sp SendParameters) error
+	// be sent. If msg is a []byte, the bytes it contains will be sent to over
+	// the connection as a single Message. If msg implements the Message
+	// interface, the Bytes() method will be invoked and the resulting []byte
+	// will be transmitted. Otherwise, msg will be passed to the framing
+	// handler's Frame() method to convert it to a []byte for transmission.
+	Send(msg interface{}, msgref interface{}, sp SendParameters) error
+
+	// Receive informs this Connection that the application is ready to
+	// receive the next message. The receiver argument is a function that will
+	// be called once per call to Receive and passed a successfully received
+	// Message on this Connection; this callback takes the Message received
+	// and the Connection upon which it was received. Note that when multiple
+	// Receive calls are pending, there is no guarantee on the order in which
+	// the callbacks will be invoked.
+	Receive(receiver func(msg Message, conn Connection))
 
 	// Clone clones this Connection, creating a new Connection to the same
 	// remote endpoint. If the underlying protocol stack supports
@@ -343,15 +414,29 @@ type Connection interface {
 
 	// SetEventHandler replaces this connection's framing handler.
 	SetFramingHandler(fh FramingHandler)
+
+	// GetTransportParameters returns this connection's current transport parameter set.
+	GetTransportParameters() TransportParameters
 }
 
-// Content provides the interface implemented by content passed to a Received
-// event.
-type Content interface {
+// Message provides the interface implemented by received Messages passed to a
+// Received event.
+type Message interface {
+	// Bytes returns the content of a Message as a byte array
 	Bytes() []byte
+
+	// Partial allows a caller to determine whether this Message represents a
+	// complete message, or part of an incomplete message. If the message is
+	// complete, Partial returns false. If the message is partial, Partial
+	// returns true, the offset into the full message of the first byte in the
+	// slice returned by Bytes(), and true if this message is not yet complete
+	// (i.e., there are Partial messages with higher offsets).
+	Partial() (bool, int, bool)
 }
 
-// EventHandler defines the interface for connection event handlers.
+// EventHandler defines the interface for connection event handlers. Every
+// asynchronous event that can occur on a Connection, except message
+// reception, is passed to an instance of this interface.
 type EventHandler interface {
 
 	// Ready occurs when a new connection is ready for use. The conn argument
@@ -367,41 +452,38 @@ type EventHandler interface {
 	// other streams.
 	Ready(conn Connection, ante Connection)
 
-	// Received occurs when Content had been received. The Content received is
-	// given as an implementation of the Content interface, on which the
-	// receiver can call Bytes(). For protocol stacks
-	Received(content Content, conn Connection)
+	// Sent occurs when a Message has been sent. The msgref argument
+	// contains the message reference given on Send().
+	Sent(conn Connection, msgref interface{})
 
-	// Sent occurs when Content has been sent. The contentref argument
-	// contains the content reference given on Send().
-	Sent(conn Connection, contentref interface{})
-
-	// Expired occurs when a Content's expires without having been sent. The
-	// contentref argument contains the content reference given on Send().
-	Expired(conn Connection, contentref interface{})
+	// Expired occurs when a Message's lifetime expires without the Message
+	// having been sent. The msgref argument contains the message reference
+	// given on Send().
+	Expired(conn Connection, msgref interface{})
 
 	// Error occurs when an error occurs on a connection. If the error refers
-	// to an attempt to send content, the contentref argument contains the
-	// content reference given on Send(). Error is only occurs for errors
-	// which do not also cause the connection to close.
-	Error(conn Connection, contentref interface{}, err error)
+	// to an attempt to send a Message, the msgref argument contains the
+	// content reference given on Send(); otherwise, it is nil. The Error
+	// event only occurs for errors which do not also cause the connection to
+	// close; Connection-closing errors will cause the Closed event to occur.
+	Error(conn Connection, msgref interface{}, err error)
 
 	// Closed occurs when a connection is closed, either actively through
 	// Close(), passively because the remote side ended the connection, or
-	// because a connection-ending error occurred. In this last case, the
+	// because a connection-ending error occurred. In this case, the
 	// error is passed as the err argument.
 	Closed(conn Connection, err error)
 }
 
 // FramingHandler defines the interface for application-assisted framing and deframing
 type FramingHandler interface {
-	// Frame converts a content object as passed to the Send() call into a
+	// Frame converts a message object as passed to the Send() call into a
 	// []byte to be passed down to the protocol stack.
-	Frame(content interface{}) ([]byte, error)
+	Frame(msg interface{}) ([]byte, error)
 
 	// Deframe reads the next object from a given reader, and returns it as an
-	// object of a type implementing Content. Deframe will only be called when
-	// receiving content via a transport protocol which does not provide its
-	// own framing (e.g. TCP)
-	Deframe(in io.Reader) (Content, error)
+	// object of a type implementing Message. Deframe will only be called when
+	// receiving data via a transport protocol which does not provide its own
+	// framing (e.g. TCP)
+	Deframe(in io.Reader) (Message, error)
 }
